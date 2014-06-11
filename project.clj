e229a9830273d25234bba2eccfbee8ad0f7ec54b
Brendan Bates <batesbrendan@gmail.com>
2014-06-11 11:14:09 +1000

    Fix project desc, upgrade cljs and core async version

--- project.clj
+++ project.clj
@@ -4,19 +4,19 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2138"]
-                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
-                 [cljs-defasync "0.1.0-SNAPSHOT"]
-]
+                 [org.clojure/clojurescript "0.0-2227" :scope "provided"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha" :scope "provided"]]
+
   :source-paths ["src/clj" "src/cljs"]
-  
+
+  :plugins [[lein-cljsbuild "1.0.3"]]
+
   :profiles {:dev {:repl-options {:init-ns cljs-idxdb.core}
                    :plugins [[lein-ring "0.8.5"]
-                             [com.cemerick/austin "0.1.3"]
-                             [lein-cljsbuild "1.0.2"]]
+                             [com.cemerick/austin "0.1.3"]]
                    :cljsbuild {:builds
-                               {:main { ;; CLJS source code path
-                                       :source-paths ["src/cljs/cljs_idxdb"]
+                               [{:main { ;; CLJS source code path
+                                        :source-paths ["src/cljs/cljs_idxdb"]
 
-                                       ;; Google Closure (CLS) options configuration
-                                       :compiler {}}}}}})
+                                        ;; Google Closure (CLS) options configuration
+                                        :compiler {}}}]}}})
